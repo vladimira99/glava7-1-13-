4773a1d28c992646ccc6f3246e3a00a01081e582
Vladimira Kamenova <44730166+vladimira99@users.noreply.github.com>
2018-12-03 11:44:58 +0200

    Add files via upload

---
+++ 7glava/7.1/7.1/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _7._1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+                int[] arr = new int[20];
+
+                for (int i = 0; i < arr.Length; i++)
+                {
+                    arr[i] = i * 5;
+                    Console.WriteLine(arr[i]);
+                }
+        }
+    }
+}
---
+++ 7glava/7.5/7.5/Program.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _7._5
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.Write("Enter array length: ");
+            int length = Int32.Parse(Console.ReadLine());
+
+            int[] arr = new int[length];
+            int sames = 1, bestSames = 1, bestStart = 0, lastElement = 0;
+
+            for (int i = 0; i < arr.Length; i++)
+            {
+                Console.Write("Enter {0} element: ", i);
+                arr[i] = Int32.Parse(Console.ReadLine());
+            }
+
+            for (int i = 0; i < arr.Length - 1; i++)
+            {
+                if (arr[i] + 1 == arr[i + 1])
+                {
+                    sames++;
+                    if (sames > bestSames)
+                    {
+                        bestSames = sames;
+                        lastElement = i + 1;
+                        bestStart = lastElement - bestSames + 1;
+                    }
+                }
+                else sames = 1;
+            }
+
+            for (int i = bestStart; i < bestSames + bestStart; i++) Console.Write("{0}, ", arr[i]);
+        }
+    }
+}
